bd79d625ae4ab72eb37914fe11537a4dace36704
Patrick Creighton <pcreighton429@gmail.com>
2022-04-11 14:11:14 -0700

    Add raye software

--- .devcontainer/config/update_bashrc.sh
+++ .devcontainer/config/update_bashrc.sh
@@ -1,6 +1,7 @@
 # Adding all the necessary ros sourcing
 echo "" >> ~/.bashrc
 echo "## ROS" >> ~/.bashrc
-echo "source /workspaces/sailbot_ros2_workspace/install/setup.bash" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 echo "export _colcon_cd_root=/workspaces/sailbot_ros2_workspace" >> ~/.bashrc
+# echo "source /workspaces/sailbot_ros2_workspace/install/setup.bash" >> ~/.bashrc
+# echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
