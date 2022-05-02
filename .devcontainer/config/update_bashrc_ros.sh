ee11525319900f7d476bb426f29c3285f2d308e5
Patrick Creighton <pcreighton429@gmail.com>
2022-05-02 02:09:04 +0000

    Move ROS workspace setup from dev to devcontainer image

--- .devcontainer/config/update_bashrc_ros.sh
+++ .devcontainer/config/update_bashrc_ros.sh
@@ -1,4 +1,4 @@
-# Adding all the necessary ros sourcing
+# Add all the necessary ros sourcing
 echo "" >> ~/.bashrc
 echo "## ROS" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
@@ -10,4 +10,5 @@ echo "source /workspaces/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "# unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "unset ROS_DISTRO" >> ~/.bashrc
 echo "# setup ros2 environment" >> ~/.bashrc
-echo "source /workspaces/sailbot_ros2_workspace/install/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/eloquent/setup.bash" >> ~/.bashrc
+echo "source /workspaces/sailbot_ros2_workspace/install/local_setup.bash" >> ~/.bashrc
