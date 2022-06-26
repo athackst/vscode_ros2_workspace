4d86ab8b610749b4496fd24bfaa56ded8b1e6a25
Patrick Creighton <pcreighton429@gmail.com>
2022-06-26 20:55:13 +0000

    Remove device-specific bash configuration and add roscd support

--- .devcontainer/config/update_bashrc_wsl.sh
+++
@@ -1,3 +0,0 @@
-# Windows: enable X11 forwarding of GUI apps
-echo "" >> ~/.bashrc
-echo "export DISPLAY=:0" >> ~/.bashrc
--- .devcontainer/dev/update_bashrc_ros.sh
+++ .devcontainer/dev/update_bashrc_ros.sh
@@ -6,9 +6,14 @@ echo "export _colcon_cd_root=/workspaces/sailbot_ros2_workspace" >> ~/.bashrc
 echo "# unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "unset ROS_DISTRO" >> ~/.bashrc
 echo "# setup ros1 environment" >> ~/.bashrc
+echo "export ROS_WORKSPACE=/workspaces/catkin_ws" >> ~/.bashrc
 echo "source /workspaces/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "# unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "unset ROS_DISTRO" >> ~/.bashrc
 echo "# setup ros2 environment" >> ~/.bashrc
 echo "source /opt/ros/eloquent/setup.bash" >> ~/.bashrc
 echo "source /workspaces/sailbot_ros2_workspace/install/local_setup.bash" >> ~/.bashrc
+
+echo "" >> ~/.bashrc
+echo "# Set up X11 forwarding for GUI apps" >> ~/.bashrc
+echo "export DISPLAY=:0" >> ~/.bashrc
