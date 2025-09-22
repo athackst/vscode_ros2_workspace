4da281a3989785a2ba9fddd319ec5b78543008e6
Ahmed Nurye <122631227+anurye@users.noreply.github.com>
2025-09-22 02:26:38 +0200

    Add file type checks for linters in run.sh (#84)
    Eliminates the need to manually remove linters from the workflow matrix.

--- .github/actions/lint/run.sh
+++ .github/actions/lint/run.sh
@@ -2,4 +2,20 @@
 set -e
 
 ./setup.sh
+
+case "$LINTER" in
+  "uncrustify"|"cppcheck"|"cpplint")
+    if [ -z "$(find src/ -name '*.cpp' -o -name '*.hpp' -o -name '*.c' -o -name '*.h')" ]; then
+      echo "No C/C++ files found. Skipping $LINTER."
+      exit 0
+    fi
+    ;;
+  "flake8"|"pep257")
+    if [ -z "$(find src/ -name '*.py')" ]; then
+      echo "No Python files found. Skipping $LINTER."
+      exit 0
+    fi
+    ;;
+esac
+
 ament_${LINTER} src/
